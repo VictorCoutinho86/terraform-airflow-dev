458c3459b61024cde36dc0b6cecd4a75d9a9d37a
Victor <victor.melo@gazeus.com>
2024-04-18 22:55:08 -0300

    feat: include new providers

--- versions.tf
+++ versions.tf
@@ -20,6 +20,16 @@ terraform {
       source  = "hashicorp/null"
       version = "3.2.2"
     }
+
+    random = {
+      source  = "hashicorp/random"
+      version = "3.6.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
+    }
   }
 
   required_version = ">= 1.0.0"
